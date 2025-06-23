5d4918891c6d7f90918de682125f4890a2956850
crookedvege <kiaora@crookedvege.co.nz>
2025-06-23 20:46:39 +1200

    Update getProgress.js

--- netlify/functions/getProgress.js
+++ netlify/functions/getProgress.js
@@ -22,6 +22,58 @@ exports.handler = async function () {
       res.on('end', () => {
         const record = JSON.parse(data);
         const fields = record.fields || {};
+        resolve({
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            progress: fields.Progress || 0,
+            current: fields['Donation Value Rollup (from Table 1)'] || 0,
+            goal: fields.Goal || 0
+          })
+        });
+      });
+    });
+
+    req.on('error', (err) => {
+      resolve({
+        statusCode: 500,
+        body: JSON.stringify({ error: err.message })
+      });
+    });
+
+    req.end();
+  });
+};
+
+
+
+const https = require('https');
+
+exports.handler = async function () {
+  const apiKey = process.env.AIRTABLE_PAT;
+  const baseId = '';
+  const table = 'Progress';
+  const recordId = 'recXwynFh17wbdqJs';
+
+  const options = {
+    hostname: 'api.airtable.com',
+    path: `/v0/${baseId}/${table}/${recordId}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${apiKey}`
+    }
+  };
+
+  return new Promise((resolve) => {
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', (chunk) => (data += chunk));
+      res.on('end', () => {
+        const record = JSON.parse(data);
+        const fields = record.fields || {};
 
         const current = fields['Donation Value Rollup (from Table 1)'] || 0;
         const goal = fields.Goal || 1; // Avoid divide-by-zero
@@ -51,4 +103,4 @@ exports.handler = async function () {
 
     req.end();
   });
-};
+}
